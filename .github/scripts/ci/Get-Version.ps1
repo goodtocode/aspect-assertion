17e324ed557d664c3f5932cc0999d751d363dc01
Robert Good <robert.good@goodtocode.com>
2026-01-19 10:36:28 -0800

    semver fixed

--- .github/scripts/ci/Get-Version.ps1
+++ .github/scripts/ci/Get-Version.ps1
@@ -1,23 +1,31 @@
 #-----------------------------------------------------------------------
-<#
-Get-Version [-VersionToReplace <String>] [-Major <String>] [-Minor <String>] [-Revision <String>] [-Build <String>] [-Patch <String>] [-PreRelease <String>] [-CommitHash <String>]
-
-Example: .\Get-Version.ps1 -Major 1 -Minor 0
-#>
+# Get-Version [-VersionToReplace <String>] [-Major <String>] [-Minor <String>] [-Revision <String>] [-Build <String>] [-Patch <String>] [-PreRelease <String>] [-CommitHash <String>]
+#
+# 1. Default (auto date/time for revision/build/patch):
+#    .\Get-Version.ps1
+#
+# 2. Set explicit major/minor (auto rest):
+#    .\Get-Version.ps1 -Major 2 -Minor 5
+#
+# 3. Full explicit version (no auto):
+#    .\Get-Version.ps1 -Major 1 -Minor 2 -Revision 3 -Build 4 -Patch 5
+#
+# 4. Pre-release and commit hash:
+#    .\Get-Version.ps1 -Major 1 -Minor 0 -Patch 1 -PreRelease -beta -CommitHash +abc123
 #-----------------------------------------------------------------------
 
 # ***
 # *** Parameters
 # ***
 param(
-    [Version] $VersionToReplace = '1.0.0',
-    [String] $Major = '-1',
-    [String] $Minor = '-1',
-    [String] $Revision = '-1',
-    [String] $Build = '-1',
-    [String] $Patch = '-1',
-    [String] $PreRelease = '-1',
-    [String] $CommitHash = '-1'
+    [string] $VersionToReplace = '1.0.0',
+    [string] $Major = '-1',
+    [string] $Minor = '-1',
+    [string] $Revision = '-1',
+    [string] $Build = '-1',
+    [string] $Patch = '-1',
+    [string] $PreRelease = '-1',
+    [string] $CommitHash = '-1'
 )
 
 # ***
@@ -33,34 +41,44 @@ param(
 # *** Execute
 # ***
 
-# Calculate version parts with defaults
-if ($Major -eq '-1') {
-    $Major = $VersionToReplace.ToString().Split('.')[0]
-}
-if ($Minor -eq '-1') {
-    $Minor = $VersionToReplace.ToString().Split('.')[1]
-}
-if ($Revision -eq '-1') {
-    $Revision = (Get-Date -UFormat '%j').ToString() # DayOfYear 1-365
-}
-if ($Build -eq '-1') {
-    $Build = (Get-Date -UFormat '%H%M').ToString() # HrMin 0000-2359
-}
-if ($Patch -eq '-1') {
-    $Patch = (Get-Date -UFormat '%m').ToString() # Month 01-12
-}
-if ($PreRelease -eq '-1') {
-    $PreRelease = ''
-}
-if ($CommitHash -eq '-1') {
-    $CommitHash = ''
+# Calculate version parts with defaults and protect against blank/null
+function Use-ValueOrDefault {
+    param($Value, $Default)
+    if ([string]::IsNullOrWhiteSpace($Value) -or $Value -eq '-1') { return $Default }
+    return $Value
 }
 
+$Major = Use-ValueOrDefault $Major (($VersionToReplace -split '\.')[0])
+if ([string]::IsNullOrWhiteSpace($Major)) { $Major = '0' }
+
+$Minor = Use-ValueOrDefault $Minor (($VersionToReplace -split '\.')[1])
+if ([string]::IsNullOrWhiteSpace($Minor)) { $Minor = '0' }
+
+$Revision = Use-ValueOrDefault $Revision ((Get-Date -UFormat '%j').ToString())
+if ([string]::IsNullOrWhiteSpace($Revision)) { $Revision = '0' }
+
+$Build = Use-ValueOrDefault $Build ((Get-Date -UFormat '%H%M').ToString())
+if ([string]::IsNullOrWhiteSpace($Build)) { $Build = '0' }
+
+$Patch = Use-ValueOrDefault $Patch ((Get-Date -UFormat '%m').ToString())
+if ([string]::IsNullOrWhiteSpace($Patch)) { $Patch = '0' }
+
+$PreRelease = Use-ValueOrDefault $PreRelease ''
+$CommitHash = Use-ValueOrDefault $CommitHash ''
+
+
+# Remove leading zeros for all numeric identifiers
+$vMajor = [int]$Major
+$vMinor = [int]$Minor
+$vRevision = [int]$Revision
+$vBuild = [int]$Build
+$vPatch = [int]$Patch
+
 # Version Formats
-$FileVersion = "$Major.$Minor.$Revision.$Build" # e.g. 1.0.0.0
-$AssemblyVersion = "$Major.$Minor.0.0"
-$InformationalVersion = "$Major.$Minor.$Revision$PreRelease$CommitHash"
-$SemanticVersion = "$Major.$Minor.$Patch$PreRelease"
+$FileVersion = "$vMajor.$vMinor.$vRevision.$vBuild" # e.g. 1.0.0.0
+$AssemblyVersion = "$vMajor.$vMinor.0.0"
+$InformationalVersion = "$vMajor.$vMinor.$vRevision$PreRelease$CommitHash"
+$SemanticVersion = "$vMajor.$vMinor.$vPatch$PreRelease"
 
 $result = [PSCustomObject]@{
 	FileVersion = $FileVersion
